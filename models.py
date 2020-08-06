8af4d3f689faa67cce07c6f6908234cb9e6dfefa
Fahad Ullah <fahadktk@mercury.cs.colostate.edu>
2020-08-06 01:30:41 -0600

    defining models layouts

---
+++ models.py
@@ -0,0 +1,17 @@
+#imports
+
+class Basset:
+    def __init__(self):
+        return
+
+class Basset_embd(Basset):
+    def __init__(self):
+        return
+    
+class Generalized:
+    def __init__(self):
+        return
+
+class Generalized_embd(Generalized):
+    def __init__(self):
+        return
